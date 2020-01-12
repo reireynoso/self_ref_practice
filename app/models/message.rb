3f25c97bd650e21dbfb8b30a10352715a1063983
reireynoso <reireynoso@gmail.com>
2020-01-11 21:40:20 -0500

    message source association

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,8 @@
 class Message < ApplicationRecord
-    belongs_to :receiver, class_name: "User"
-    belongs_to :sender, class_name: "User"
+
+    belongs_to :person_sending_the_message, class_name: "User", foreign_key: "sender_id"
+    belongs_to :person_receving_the_message, class_name: "User", foreign_key: "receiver_id"
+    # belongs_to :receiver, class_name: "User"
+    # belongs_to :sender, class_name: "User"
 
 end
