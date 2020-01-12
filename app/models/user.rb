feabce204a98e65d27e67446578e8d7c3caccc23
reireynoso <reireynoso@gmail.com>
2020-01-11 21:45:08 -0500

    alternative assocation for user and message

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,14 @@ class User < ApplicationRecord
     # has_many :followers, through: :following_users
 
     has_many :sent_messages, foreign_key: :sender_id, class_name: "Message"
-    has_many :receivers, through: :sent_messages
+    has_many :receivers, through: :sent_messages, source: :person_receving_the_message
 
     has_many :received_messages, foreign_key: :receiver_id, class_name: "Message"
-    has_many :senders, through: :received_messages
+    has_many :senders, through: :received_messages, source: :person_sending_the_message
+
+    # has_many :sent_messages, foreign_key: :sender_id, class_name: "Message"
+    # has_many :receivers, through: :sent_messages
+
+    # has_many :received_messages, foreign_key: :receiver_id, class_name: "Message"
+    # has_many :senders, through: :received_messages
 end
