105702e4a54c20b1b5ccdfb6629b04802586d79e
reireynoso <reireynoso@gmail.com>
2020-01-11 20:52:30 -0500

    alternative assocation with source

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,4 @@
 class Follow < ApplicationRecord
-    belongs_to :follower, class_name: "User"
-    belongs_to :followee, class_name: "User"
+    belongs_to :person_doing_the_following, class_name: "User", foreign_key: "follower_id"
+    belongs_to :person_being_followed, class_name: "User", foreign_key: "followee_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
 
     has_many :followed_users, foreign_key: :follower_id, class_name: "Follow"
-    has_many :followees, through: :followed_users
+    has_many :followees, through: :followed_users, source: :person_being_followed
 
     has_many :following_users, foreign_key: :followee_id, class_name: "Follow"
-    has_many :followers, through: :following_users
+    has_many :followers, through: :following_users, source: :person_doing_the_following
+
+    # has_many :followed_users, foreign_key: :follower_id, class_name: "Follow"
+    # has_many :followees, through: :followed_users
+
+    # has_many :following_users, foreign_key: :followee_id, class_name: "Follow"
+    # has_many :followers, through: :following_users
 
     has_many :sent_messages, foreign_key: :sender_id, class_name: "Message"
     has_many :receivers, through: :sent_messages
